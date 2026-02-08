a39fe19acf3dc382046bdce1e5542190759545f4
Denis Prokhorenko <denis.prokhorenko1@gmail.com>
2026-02-08 21:34:08 +0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,23 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 import cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
 
   app.use(cookieParser());
 
   app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors({
+    origin: config.getOrThrow<string>('ALLOWED_ORIGINS').split(','),
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    exposedHeaders: ['Set-Cookie', 'Content-Disposition'],
+    allowHeaders: ['Authorization', 'X-Api-Key'],
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
